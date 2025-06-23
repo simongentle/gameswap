455616ef24422035839a8d72d312bae586201fb8
Simon Gentle <simongentle08@gmail.com>
2025-06-23 17:43:19 +0200

    Added schema for game using Pydantic.

---
+++ app/schemas/game.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class GameBase(BaseModel):
+    title: str
+    platform: str
+
+
+class GameCreate(GameBase):
+    pass
+
+
+class GameUpdate(GameBase):
+    pass
+
+
+class Game(GameBase):
+    id: int
+    title: str
+    platform: str
