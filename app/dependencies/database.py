3dd6bbd089f82ec49aedfe41cbf820fbf28e0d06
Simon Gentle <simongentle08@gmail.com>
2025-06-26 08:24:23 +0200

    Defined database table creation and session handling.

---
+++ app/dependencies/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.game import Base
+
+
+DBSession = sessionmaker(autocommit=False, autoflush=False)
+
+
+def init_db(file: str):
+    engine = create_engine(file, connect_args={"check_same_thread": False})
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db_session():
+    db = DBSession()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/routers/games.py
+++ app/routers/games.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 import app.crud.games as games
+from app.dependencies.database import get_db_session
 from app.schemas.game import Game, GameCreate, GameUpdate
 
 
@@ -8,12 +10,12 @@ router = APIRouter()
 
 
 @router.post("/games", response_model=Game)
-def create_game(game: GameCreate, session) -> Game:
+def create_game(game: GameCreate, session: Session = Depends(get_db_session)) -> Game:
     return games.create_game(session, game)
 
 
 @router.get("/games", response_model=list[Game]) 
-def get_games(session) -> list[Game]:
+def get_games(session: Session = Depends(get_db_session)) -> list[Game]:
     return games.get_games(session)
 
 
@@ -23,11 +25,10 @@ def get_game(game_id: int, session) -> Game:
 
 
 @router.patch("/games/{game_id}", response_model=Game)
-def update_game(game_id: int, params: GameUpdate, session) -> Game:
+def update_game(game_id: int, params: GameUpdate, session: Session = Depends(get_db_session)) -> Game:
     return games.update_game(session, game_id, params)
 
 
 @router.delete("/games/{game_id}", response_model=Game) 
-def delete_game(game_id: int, session) -> Game:
+def delete_game(game_id: int, session: Session = Depends(get_db_session)) -> Game:
     return games.delete_game(session, game_id)
-
