4a4ed47712788d905a139baf843de36ac861f55a
Simon Gentle <simongentle08@gmail.com>
2025-06-26 07:58:43 +0200

    Added endpoints for a FastAPI app. Session dependency not yet specified.

---
+++ app/routers/games.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+
+import app.crud.games as games
+from app.schemas.game import Game, GameCreate, GameUpdate
+
+
+router = APIRouter()
+
+
+@router.post("/games", response_model=Game)
+def create_game(game: GameCreate, session) -> Game:
+    return games.create_game(session, game)
+
+
+@router.get("/games", response_model=list[Game]) 
+def get_games(session) -> list[Game]:
+    return games.get_games(session)
+
+
+@router.get("/games/{game_id}", response_model=Game) 
+def get_game(game_id: int, session) -> Game:
+    return games.get_game(session, game_id)
+
+
+@router.patch("/games/{game_id}", response_model=Game)
+def update_game(game_id: int, params: GameUpdate, session) -> Game:
+    return games.update_game(session, game_id, params)
+
+
+@router.delete("/games/{game_id}", response_model=Game) 
+def delete_game(game_id: int, session) -> Game:
+    return games.delete_game(session, game_id)
+
