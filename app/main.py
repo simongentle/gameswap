4360222fbcb5b5fa5cc8d7c75883f66cdcb42fab
Simon Gentle <simongentle08@gmail.com>
2025-06-27 17:12:52 +0200

    Added OpenAPI documentation.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from app.dependencies.database import init_db
 from app.routers import games
 
 
+PROJECT_NAME = "gameswap"
+PROJECT_SUMMARY = "track game swaps with friends"
 DB_FILE = "sqlite:///gameswap.db"
 
 
@@ -15,15 +17,19 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title=PROJECT_NAME, 
+    summary=PROJECT_SUMMARY,
+    lifespan=lifespan,
+)
 
 
-@app.get("/")
+@app.get("/", tags=["root"])
 def read_root():
     return "The server is running."
 
 
-app.include_router(games.router)
+app.include_router(games.router, tags=["games"])
 
 
 def main():
