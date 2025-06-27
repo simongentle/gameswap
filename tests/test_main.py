dc9fc63a7f2e0ab6cf8e59f3122245328f050fc2
Simon Gentle <simongentle08@gmail.com>
2025-06-27 09:19:52 +0200

    Updated main test: tests root.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,12 @@
-def test_main() -> None:
-    assert True
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_read_root() -> None:
+    response = client.get("/")
+    assert response.status_code == 200
     
