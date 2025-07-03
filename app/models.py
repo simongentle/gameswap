7d04c24705991d4659663f6fc73ef8d9e5f112c5
Simon Gentle <simongentle08@gmail.com>
2025-07-03 20:48:41 +0200

    Added swap table with relationship to game table: a game can be related to one swap, and a swap can be related to many games. Existing tests pass. No other chnages.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+import datetime as dt
 from enum import StrEnum
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -18,3 +19,15 @@ class Game(Base):
     title: Mapped[str]
     platform: Mapped[str]
     status: Mapped[str] = mapped_column(default=Status.OWNED.value)
+
+    swap_id: Mapped[int | None] = mapped_column(ForeignKey("swap.id"))
+    swap: Mapped["Swap | None"] = relationship(back_populates="games")
+
+
+class Swap(Base):
+    __tablename__ = "swap"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    friend: Mapped[str]
+    return_date: Mapped[dt.date] 
+
+    games: Mapped[list[Game]] = relationship(back_populates="swap")
