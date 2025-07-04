73e9e2afcdfddf755569ee2d5c6d02d4c7762bd2
Simon Gentle <simongentle08@gmail.com>
2025-07-04 14:16:31 +0200

    Moved common fixtures to a conftest file.

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+
+from fastapi.testclient import TestClient
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import Session
+
+from app.models import Base
+from app.main import app
+from app.dependencies.database import get_session
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    Base.metadata.create_all(engine)
+
+    with Session(engine, autocommit=False, autoflush=False) as session:
+        yield session
+
+
+@pytest.fixture(name="client")  
+def client_fixture(session: Session):  
+    def get_session_override():  
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override  
+
+    client = TestClient(app)  
+    yield client  
+    app.dependency_overrides.clear()  
+    
--- tests/test_games.py
+++ tests/test_games.py
@@ -1,35 +1,7 @@
-import pytest
-
 from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import Session
 
-from app.models import Base, Status, Game as DBGame
-from app.main import app
-from app.dependencies.database import get_session
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    Base.metadata.create_all(engine)
-
-    with Session(engine, autocommit=False, autoflush=False) as session:
-        yield session
-
-
-@pytest.fixture(name="client")  
-def client_fixture(session: Session):  
-    def get_session_override():  
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override  
-
-    client = TestClient(app)  
-    yield client  
-    app.dependency_overrides.clear()  
+from app.models import Status, Game as DBGame
 
 
 def test_create_game(client: TestClient) -> None:
