9afcbe965aaaad1b9a604e495b1f9ec46488bcd1
Simon Gentle <simongentle08@gmail.com>
2025-07-04 12:45:34 +0200

    Added swap CRUD.

---
+++ app/crud/swaps.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+
+from app.models import Swap as DBSwap
+from app.schemas.swap import Swap, SwapCreate, SwapUpdate
+
+
+class SwapNotFoundError(Exception):
+    pass
+
+
+def get_swap(session: Session, swap_id: int) -> Swap:
+    swap = session.get(DBSwap, swap_id)
+    if swap is None:
+        raise SwapNotFoundError
+    return swap
+
+
+def get_swaps(session: Session) -> list[Swap]:
+    swaps = session.query(DBSwap).all()
+    return swaps
+
+
+def create_swap(session: Session, params: SwapCreate) -> Swap:
+    swap = DBSwap(**params.model_dump())
+    session.add(swap)
+    session.commit()
+    session.refresh(swap)
+    return swap
+    
+
+def update_swap(session: Session, swap_id: int, params: SwapUpdate) -> Swap:
+    swap = get_swap(session, swap_id)
+    for attr, value in params.model_dump(exclude_unset=True).items():
+        setattr(swap, attr, value)
+    session.add(swap)
+    session.commit()
+    session.refresh(swap)
+    return swap
+
+
+def delete_swap(session: Session, swap_id: int) -> Swap:    
+    swap = get_swap(session, swap_id)
+    session.delete(swap)
+    session.commit()
+    return swap
