3e5edbc7e2e51d7a77f59f35433b1d5a5acede8a
Simon Gentle <simongentle08@gmail.com>
2025-09-21 10:57:35 +0200

    Added swap table with relationships to game and gamer tables. Return date field and validation not included.

--- app/models.py
+++ app/models.py
@@ -16,6 +16,9 @@ class Game(Base):
     gamer_id: Mapped[int] = mapped_column(ForeignKey("gamer.id", ondelete="CASCADE"))
     gamer: Mapped["Gamer"] = relationship(back_populates="games")
 
+    swap_id: Mapped[int | None] = mapped_column(ForeignKey("swap.id", ondelete="SET NULL"))
+    swap: Mapped["Swap | None"] = relationship(back_populates="games")
+
 
 class Gamer(Base):
     __tablename__ = "gamer"
@@ -24,3 +27,19 @@ class Gamer(Base):
     email: Mapped[str] = mapped_column(unique=True)
 
     games: Mapped[list[Game]] = relationship(back_populates="gamer", cascade="all, delete-orphan")
+
+    proposer_swaps: Mapped[list["Swap"]] = relationship(back_populates="proposer", foreign_keys="Swap.proposer_id")
+    acceptor_swaps: Mapped[list["Swap"]] = relationship(back_populates="acceptor", foreign_keys="Swap.acceptor_id")
+
+
+class Swap(Base):
+    __tablename__ = "swap"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    games: Mapped[list[Game]] = relationship(back_populates="swap")
+
+    proposer_id: Mapped[int] = mapped_column(ForeignKey("gamer.id"))
+    proposer: Mapped[Gamer] = relationship(back_populates="proposer_swaps", foreign_keys=proposer_id)
+
+    acceptor_id: Mapped[int] = mapped_column(ForeignKey("gamer.id"))
+    acceptor: Mapped[Gamer] = relationship(back_populates="acceptor_swaps", foreign_keys=acceptor_id)
